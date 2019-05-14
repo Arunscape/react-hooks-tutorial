35a43869b4c98af87dce889d9ac885948440525f
ArunW <awoosaree@atb.com>
2019-05-14 13:38:11 -0600

    useeffect hoock

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,27 @@ const App: React.FC = () => {
   const [count, setCount] = useState(0)
   const [isOn, toggleOn] = useState(false)
 
+  interface mousePosition {
+    x: number,
+    y: number
+  }
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 })
+
+
+  const handleMouseMove = (event: { pageX: number, pageY: number }) => setMousePosition({ x: event.pageX, y: event.pageY })
 
   // useeffect is called on every render
   useEffect(() => {
     document.title = `You clicked ${count} times`
-  })
+
+    window.addEventListener('mousemove', handleMouseMove)
+
+    // tell useeffect to do something before unmount, works like componentWillUnmount on steroids
+    // will run when the component unmounts AND before re-render
+    return () => window.removeEventListener('mousemove', handleMouseMove)
+  },
+    [count] // empty list will only run component mount and unmount but now it also run when count changes
+  )
 
   return (
     <>
@@ -28,6 +44,9 @@ const App: React.FC = () => {
         onClick={() => toggleOn((prev) => !prev)}
         alt="Flashlight"
       />
+      <h2>Mouse Position</h2>
+      <p>X position: {mousePosition.x}</p>
+      <p>Y position: {mousePosition.y}</p>
     </>
   )
 
