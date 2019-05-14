999c7661316a74ec75c1add2a55ddbcda9e01f01
ArunW <awoosaree@atb.com>
2019-05-14 11:43:41 -0600

    nice I did it before the tutorial got to it

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,25 @@ import React, { useState } from 'react'
 const App: React.FC = () => {
 
   const [count, setCount] = useState(0)
+  const [isOn, toggleOn] = useState(false)
 
   return (
-    <button onClick={() => setCount((prevCount) => prevCount + 1)}>
-      I was clicked {count} times!
-        </button>
+    <>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        I was clicked {count} times!
+      </button>
+      <h2>Toggle this</h2>
+      <div style={{
+        height: "50px",
+        width: "50px",
+        background: isOn ? "yellow" : "grey"
+      }}
+        onClick={() => toggleOn((prev) => !prev)}>
+
+      </div>
+    </>
   )
+
 }
 
 export default App;
