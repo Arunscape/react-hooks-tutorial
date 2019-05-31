311c5a6bf12293c2ef7b5518bffe87ce8687932f
ArunW <awoosaree@atb.com>
2019-05-31 13:48:59 -0600

    ooh nice we have a list of things

--- hooks-news/src/App.tsx
+++ hooks-news/src/App.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
-
+interface result {
+  url: string,
+  title: string,
+  objectID: any
+}
 
 const App: React.FC = () => {
+
+
+
+  const [results, setResults] = useState<result[]>([])
+  useEffect(() => {
+    axios.get('https://hn.algolia.com/api/v1/search?query=reacthooks')
+      .then(res => res.data)
+      .then(data => setResults(data.hits))
+
+
+  }, [])
+
   return (
     <>
-
+      <ul>
+        {results.map(r => (
+          <li key={r.objectID}>
+            <a href={r.url}> {r.title}</a>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
