f0995da5c73dd5ab193a500a07058eb95fc8a1e6
ArunW <awoosaree@atb.com>
2019-05-31 14:02:51 -0600

    the search results update as you type

--- hooks-news/src/App.tsx
+++ hooks-news/src/App.tsx
@@ -10,18 +10,24 @@ interface result {
 const App: React.FC = () => {
 
 
-
+  const [query, setQuery] = useState<string>('react')
   const [results, setResults] = useState<result[]>([])
   useEffect(() => {
-    axios.get('https://hn.algolia.com/api/v1/search?query=reacthooks')
-      .then(res => res.data)
-      .then(data => setResults(data.hits))
+    // axios.get('https://hn.algolia.com/api/v1/search?query=reacthooks')
+    //   .then(res => res.data)
+    //   .then(data => setResults(data.hits))
+    getResults()
 
+  }, [query]) //updates on component mount and when query changes
 
-  }, [])
+  const getResults = async () => {
+    const res = await axios.get(`https://hn.algolia.com/api/v1/search?query=${query}`)
+    setResults(res.data.hits)
+  }
 
   return (
     <>
+      <input type="text" onChange={event => setQuery(event.target.value)} />
       <ul>
         {results.map(r => (
           <li key={r.objectID}>
